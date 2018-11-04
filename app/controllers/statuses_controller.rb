62dd2f6fb93ec2e553744a17f01136d1f270615d
ReoFujishima <jplowling@gmail.com>
2018-11-04 04:15:33 -0800

    added img

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,16 +2,17 @@ class StatusesController < InheritedResources::Base
   # before_action :authenticate_hospital!, only: [:new, :create, :edit]
   before_action :authenticate_user!, only: [:show]
 
-  # def create
-  #   # @status = Status.new(hospital_id: current_hospital.id, value: params[:value], user_id: params[:user_id])
-  #   # @status.hospital_id = current_hospital.id
-  #   # @status = Status.find(params[:id])
-  #   # @status.hospital_id = current_hospital.id
-  #   # @status = params[:status]
-  #   # @status = Status.new(params[:status])
-  #   # create!(:notice => "New Status was created") { root_url }
-  #   create!(:notice => "New Status was created")
-  # end
+  def create
+    # @status = Status.new(hospital_id: current_hospital.id, value: params[:value], user_id: params[:user_id])
+    # @status.hospital_id = current_hospital.id
+    # @status = Status.find(params[:id])
+    # @status.hospital_id = current_hospital.id
+    # @status = params[:status]
+    # @status = Status.new(params[:status])
+    # create!(:notice => "New Status was created") { root_url }
+    create!(:notice => "New Status was created")
+  end
+
   def show
     @status = Status.find(params[:id])
     if @status.user_id == current_user.uuid
