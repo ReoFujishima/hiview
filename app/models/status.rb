b1a0bdeb89fae7a7d40f481b502f99c310a79b22
ReoFujishima <jplowling@gmail.com>
2018-11-12 14:43:47 +0900

    add schema on Models

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id          :integer          not null, primary key
+#  value       :string
+#  user_id     :string
+#  hospital_id :string
+#  image_id    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Status < ApplicationRecord
   belongs_to :user
   belongs_to :hospital
