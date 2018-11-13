138e8f5ea6bf3762bc9bd6a54507768f97492de5
ReoFujishima <jplowling@gmail.com>
2018-11-13 11:47:02 +0900

    changed the primary key of users

---
+++ db/migrate/20181113020413_add_reference_to_status.rb
@@ -0,0 +1,8 @@
+class AddReferenceToStatus < ActiveRecord::Migration[5.2]
+  def change
+    # change_table :statuses do |t|
+    #   # t.タイプ カラム名 [, オプション]
+    #   # t.references :user, type: :uuid
+    # end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_064144) do
+ActiveRecord::Schema.define(version: 2018_11_13_020413) do
 
   create_table "hospitals", force: :cascade do |t|
     t.string "email", default: "", null: false
